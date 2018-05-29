965e65d7a8f27f56146f012af68f391e7c8c9329
John Higgins <johnhiggins@outlook.ie>
2018-05-29 14:31:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ function logWhisper(){
 }
 
 function sayHiToGrandma (string){
-   if (string.toLowerCase === string){
-     return ('I can\'t hear you!');
+   if (string.toLowerCase() === string){
+     return "I can't hear you!!";
     } 
     
-    if (string.toUpperCase === string) {
-     return ('YES INDEED!');
+    if (string.toUpperCase() === string) {
+     return "YES INDEED!";
     }
     
     if (string === "I love you, Grandma.") {
